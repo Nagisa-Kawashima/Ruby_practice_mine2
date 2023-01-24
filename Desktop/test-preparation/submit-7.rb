dfd4f53517adf7b9b994d3bd2046cc1e29d5857f
kawashima nagisa <kawashima.it333@outlook.com>
2023-01-24 15:54:28 +0900

    Edit

--- Desktop/test-preparation/submit-7.rb
+++ Desktop/test-preparation/submit-7.rb
@@ -47,3 +47,134 @@ p b
 a = [1]
 b = [2,3] 
 p a + b
+
+
+a = [1,2,3] 
+b = [3,4,5] 
+
+p a | b
+
+a = [1,2,3] 
+b = [3,4,5] 
+p a - b 
+
+a = [1,2,3] 
+b = [3,4,5] 
+p a & b
+
+
+require 'set'
+a = Set[1,2,3]
+b = Set[3,4,5] 
+
+p a | b 
+p a - b 
+p a & b
+
+e, f = 100, 200, 300 
+p e 
+p f 
+
+e, *f = 100, 200, 300 
+p e 
+p f
+
+e, * = 100, 200, 300 
+p e  
+
+e, = 100, 200, 300 
+p e  
+
+
+a, *b, c, d = 1,2,3,4,5 
+p a
+p b
+p c
+p d 
+
+a, *b, c, d = 1,2,3 
+p a
+p b
+p c
+p d 
+
+
+a = [] 
+b = [2,3]
+p a.push(1) 
+# p a.push(b) 
+p a.push(*b)
+p a  
+
+jp = ['japan', '日本']
+country = '日本'
+case country
+when *jp 
+    p 'こんにちは'
+end 
+
+
+def greet(*names)
+    "#{names.join('と')},こんにちは！"
+end 
+
+p greet('田中さん') 
+p greet('田中さん', '鈴木さん', '佐藤さん')
+
+a, *b, c, d = 1,2,3,4,5
+p a 
+p b
+p c
+p d  
+
+def foo(a, *b, c, d)
+    p "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
+end 
+
+
+foo(1,2,3,4,5) 
+
+a = [1,2,3] 
+p [a] 
+p [*a] 
+
+a = [1,2,3,4]
+p [-1, 0, *a, 4, 5] 
+
+a = [1,2,3] 
+p [-1, 0] + a + [4,5]
+
+p [1, 2, 3] == [1, 2, 3]
+p [1, 2, 3] == [1, 2, -3]
+p [1, 2, 3] == [1, 2, 3, 4, 5]
+ 
+p ['apple', 'melon', 'orange']
+
+p %W!apple melon orange  !
+p %W(apple melon orange)
+p %w(apple melon orange)
+p %W(
+    apple
+    melon
+    orange
+) 
+
+p %W(big\ apple small\ melon orange) 
+
+prefix = 'This is'
+p %W(#{prefix}\ an\ apple small \nmelon orange) 
+
+
+puts %q!He said, "Don't speak."! 
+
+
+p 'Ruby'.chars 
+
+p "ruby, java, python".split(',') 
+
+
+a = Array.new(6, 3) 
+p a 
+
+a = Array.new(10) { |n| n % 3 + 1} 
+p a
