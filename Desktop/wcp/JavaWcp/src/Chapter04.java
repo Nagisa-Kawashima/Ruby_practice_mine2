696b790b77a6dfb639147933ca382d59904a6c33
kawashima nagisa <kawashima.it333@outlook.com>
2023-01-25 23:19:29 +0900

    study java

---
+++ Desktop/wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+//		定数の制限
+		String str = "川島　渚"; 
+		System.out.println(str);
+		str = "少年よ大使を抱け"; 
+		System.out.println(str); 
+		final int LUKEY_NUMBER = 7;
+		System.out.println(LUKEY_NUMBER);
+		
+	}
+	
+	
+	
+}
