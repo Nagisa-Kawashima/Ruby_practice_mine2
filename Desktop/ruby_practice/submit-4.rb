7f9ad75a593fb07f34f0922505533967f13261a7
kawashima nagisa <kawashima.it333@outlook.com>
2023-02-09 15:05:49 +0900

    Add submit-4

---
+++ Desktop/ruby_practice/submit-4.rb
@@ -0,0 +1,8 @@
+currencies = { japan: 'ten', us: 'dollar', india: 'rupee'}
+p currencies.keys
+currencies = { japan: 'ten', us: 'dollar', india: 'rupee'}
+p currencies.values 
+
+currencies = { japan: 'ten', us: 'dollar', india: 'rupee'}
+p currencies.has_key?(:japan)
+p currencies.has_key?(:italy)
