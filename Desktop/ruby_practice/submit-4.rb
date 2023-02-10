2e63901f86aeaa1369cb9b2777d0634c9ce6bab7
kawashima nagisa <kawashima.it333@outlook.com>
2023-02-10 21:29:48 +0900

    Edit

--- Desktop/ruby_practice/submit-4.rb
+++ Desktop/ruby_practice/submit-4.rb
@@ -124,3 +124,122 @@ bar({x:1})
 #     bar(...)
 # end 
 # foo(10, 20, c:30)
+
+# def foo(*args, **opts)
+#     bar(*args, **opts)
+# end 
+def foo(...)
+    bar(...)
+end 
+
+def bar(a, b, c: 1)
+    puts "a=#{a}, b = #{b}, c= #{c}"
+end 
+
+foo(10, 20, c:30)
+
+
+def foo(a, ...)
+    bar(a * 100, ...)
+end 
+
+def bar(a, b, c: 1)
+    puts "a=#{a}, b = #{b}, c= #{c}"
+end 
+
+foo(10, 20, c:30) 
+
+:apple
+:Apple 
+:ruby_is_fun
+:okay?
+:welcome! 
+:_select
+:$dollar
+:@at_mark 
+:+
+:==
+:'12345'
+:'ruby_is_fun'
+:'ruby is fun'
+:'()'
+
+name = 'Alice'
+p :"#{name.upcase}"
+
+hash = { 'abc': 123 }
+p hash 
+
+p %s!ruby is fun!
+p %s(ruby is fun)
+p %i(apple orange melon) 
+
+name = 'Alice'
+p %i(hello\ngood-bye #{name.upcase})
+p %I(hello\ngood-bye #{name.upcase}) 
+
+# String = 'apple'
+# Symbol = :apple 
+# p String == Symbol
+# # p String + Symbol 
+
+# String = 'apple'
+# Symbol = :apple 
+# p String.to_sym 
+# p String.to_sym == Symbol 
+
+String = 'apple'
+Symbol = :apple 
+# p Symbol.to_s 
+p Symbol.id2name 
+p String == Symbol.id2name 
+p String + Symbol.id2name
+
+p 'apple'.respond_to?('include?')
+p 'apple'.respond_to?(:include?)
+p 'apple'.respond_to?('foo_bar')
+p 'apple'.respond_to?(:foo_bar) 
+
+p 'apple'.include?('pp')
+# p 'apple'.include?(:pp) 
+
+def foo(a, b, c = 3, d = 4, *ef, g, h, i: 9, j: 10, **kl, &block)
+    "a: #{a}, b: #{b}, c: #{c}, d: #{d}, ef: #{ef}, g: #{g}, h: #{h}, i: #{i}, j: #{j}, kl: #{kl}, block: #{block} "
+end
+p foo(1,2,3,4,5,6,7,8, i:9, j:10, k:11, l:12){ 13 } 
+
+def find_currency(country)
+    currencies = { japan: 'yen', us: 'dollar', india: 'rupee'}
+    currencies[country]
+end 
+
+def show_currency(country)
+    currency = find_currency(country)
+    currency&.upcase
+    # if currency 
+    #     currency.upcase 
+    # end 
+end 
+
+p show_currency(:japan)
+p show_currency(:brazil) 
+
+a = 'foo'
+p a&.upcase 
+
+
+
+a = nil 
+p a&.upcase
+
+limit = nil
+limit ||= 10 
+p limit  
+
+limit = nil 
+limit ||= begin 
+    a = 10
+    b = 20
+    p a + b 
+end
+limit
