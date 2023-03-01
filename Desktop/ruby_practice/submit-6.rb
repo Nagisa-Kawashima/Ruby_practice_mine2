20cb554c2fa7deaa3d61baae4c4e33bdca290034
kawashima nagisa <kawashima.it333@outlook.com>
2023-03-01 21:25:01 +0900

    Edit

--- Desktop/ruby_practice/submit-6.rb
+++ Desktop/ruby_practice/submit-6.rb
@@ -64,3 +64,157 @@ team.push(Warrior.new("戦士"))
 team.each do |person|
     person.attack("スライム")
 end
+
+# クラスからメソッドを呼び出す
+
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+class Wizard < Player
+    def initialize()
+        # @name = "魔法使い"
+        super("魔法使い")
+    end
+    def attack(enemy)
+        # puts "シャラララーン！"
+        spell()
+        puts "#{@name}は、#{enemy}に炎を放った！"
+    end
+    private
+    def spell()
+        puts "シャラララーン！"
+    end
+end
+
+
+puts "=== パーティでスライムと戦う ==="
+hero   = Player.new("勇者")
+warrior = Player.new("戦士")
+wizard = Wizard.new
+party = [hero, warrior, wizard]
+
+party.each do |player|
+    player.attack("スライム")
+end
+# wizard.spell()
+
+
+# クラス変数とクラスメソッド
+
+class Player
+    @@character_count = 0
+    def self.character_count()#オブジェクトなしで直接クラスを
+    # 呼び出せるから　クラスメソッド
+        @@character_count
+    end
+    def initialize(name)
+        @name = name
+        @@character_count += 1
+        puts "#{@@character_count}番目のプレイヤー#{name}が登場した"
+    end
+
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+class Wizard < Player
+    def initialize()
+        super("魔法使い")
+    end
+
+    def attack(enemy)
+        spell()
+        puts "#{@name}は、#{enemy}に炎を放った！"
+    end
+
+    private
+
+    def spell()
+        puts "シャラララーン"
+    end
+end
+
+
+puts "=== パーティでスライムと戦う ==="
+hero   = Player.new("勇者")
+warrior = Player.new("戦士")
+wizard = Wizard.new()
+party = [hero, warrior, wizard]
+
+party.each do |player|
+    player.attack("スライム")
+end
+puts "#{Player.character_count()}人でスライムを攻撃した" #クラスメソッドの呼び出し
+
+class Greeting
+    def self.say_hello()
+        puts "hello paiza"
+    end
+end
+
+# この下で、クラスメソッドを呼び出す
+Greeting.say_hello()
+
+
+# Here your code !
+#クラスメソッド
+
+today = Time.now()
+puts today.year
+puts today.month 
+puts today.day
+
+
+require "time"
+
+time= Time.parse("15.07.10") do |year|
+    if year < 100 
+        if year >= 69 
+            year + 1900 
+        else
+            year + 2000 
+        end 
+    else  
+        year 
+    end
+    
+end
+
+
+
+# Timeクラスのparseメソッド
+# time = Time.parse("2017-07-10 12:00:00")
+# time = Time.parse("2017-07-10")
+# time = Time.parse("2017/07/10")
+# time = Time.parse("2017.07.10")
+# puts time
+
+# - 組み込みライブラリ：プログラミングの標準的な機能を提供。「require」メソッドなしで利用できる。
+# - 標準添付ライブラリ：Rubyといっしょに配布。「require」メソッドで利用する。
+# - RubyGem：Rubyライブラリのホスティングサービス。「gem」コマンドでインストールして利用する。
+
+require 'date'
+
+ a = Date.new(1993, 2, 24)
+ b = Date.parse('1993-02-24')
+b += 10
+p b
+
+p b - a            #=> 10
+p b.year           #=> 1993
+p b.strftime('%a') #=> "Sat"
+
+yesterday = Date.today - 1
+p yesterday
+
+
+
+# モジュールは、Rubyの定数やメソッドだけをまとめる機能です。モジュールを使うことで、共通の定数やメソッドをクラスとは分離して共有することができます。
