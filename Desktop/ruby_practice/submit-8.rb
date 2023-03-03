3497da2cad7acb72889b6bd26a5f29fa9fbd9d5d
kawashima nagisa <kawashima.it333@outlook.com>
2023-03-03 14:16:16 +0900

    Edit

--- Desktop/ruby_practice/submit-8.rb
+++ Desktop/ruby_practice/submit-8.rb
@@ -90,3 +90,140 @@ ensure
     puts "勇者は勝利した"
 end
 
+puts 1
+begin
+    # number = 1
+    puts 2   
+    raise Exception.new("意図的な例外")   
+    puts 3
+    # answer = 100 / number
+    # puts answer
+rescue Exception => e
+    puts "予期せぬエラーが発生しました"
+    p e
+    puts e.message
+    
+ensure
+    puts 4
+end
+
+
+
+puts "Hello World"
+
+begin
+    # raise ZeroDivisionError.new("強制エラー")
+    raise Exception.new("0では割り算できません")
+rescue Exception => e
+    # STDERR.puts "未定義の変数を呼び出しています"
+    STDERR.puts e.message
+    # puts e.message
+ensure
+   puts "Hello Ruby"
+end
+
+
+puts "Hello Word"
+
+begin 
+    raise ZeroDivisionError.new("強制エラー")
+rescue NameError 
+    STDERR.puts "未定義の変数を呼び出しています"
+rescue ZeroDivisionError  
+    STDERR.puts "0では割り算できません"
+ensure   
+    puts "Hello Ruby"
+end
+
+# raiseを使うと、意図的に例外処理を起動できます。また、rescueと組み合わせることで、メソッドの呼び出し元にある例外処理を利用できます。
+
+def test_exception(number)
+    puts 2
+    begin   
+        puts 3
+        answer = 100 / number
+        return answer
+        puts 4    
+    rescue ZeroDivisionError => e 
+        puts 5    
+        raise  e
+    end
+    puts 6
+end
+
+puts 1
+begin
+    answer = test_exception(0)
+    puts 7
+    # puts 3 #このメソッドないで例外が発生したので、3は出力されない
+rescue ZeroDivisionError => e        
+    puts 8 
+    p e      
+end
+# ------------------------------------------------------------------
+
+def calc()
+    begin
+        number = 100 / 0
+    rescue ZeroDivisionError => e 
+        raise  e
+    end
+end
+
+begin
+    puts calc()
+rescue ZeroDivisionError => e        
+    STDERR.puts "0で割り算できません"
+end
+
+# ----------------------------------------------------
+def test_exception(number)
+    puts 2
+    begin
+        puts 3
+        answer = 100 / number
+        return answer
+        puts 4
+    rescue ZeroDivisionError => e
+        puts 5
+        raise e
+    ensure
+        puts 6
+    end
+ 
+    # puts 6
+end
+
+puts 1
+
+begin
+    answer = test_exception(0)
+    puts 7
+rescue ZeroDivisionError => e
+    puts 8
+    p e
+end
+
+
+#例外が発生した、していない場合でも必ず実行したい処理はensureブロック
+
+# --------------------------------------------------
+
+def test_exception(number)
+    begin
+        answer = 100 / number  
+        return answer
+    rescue ZeroDivisionError => e       
+        raise e   
+    ensure 
+        puts "処理が終了しました"
+        
+    end
+end
+
+
+begin   
+    puts test_exception(0)
+rescue ZeroDivisionError => e     
+    STDERR.puts "0では割り算できません"
+end
