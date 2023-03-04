a9eb7c1b343f0fb412ee8dedad412e9556e1ecf8
kawashima nagisa <kawashima.it333@outlook.com>
2023-03-04 22:05:12 +0900

    Edit

--- Desktop/ruby_practice/submit-10.rb
+++ Desktop/ruby_practice/submit-10.rb
@@ -121,3 +121,14 @@ a.each { |row| puts row.join(' ') }
 a = 3.times.map { gets.split.map(&:to_i) }
 
 a.each { |row| puts row.join(' ') }
+# ---------------------------------------------
+
+input_line = gets.to_i
+# puts "XXXXXX"
+
+num = gets
+array = num.split
+array.each do |line|   
+    puts line
+end
+# p array
--- Desktop/ruby_practice/submit-11.rb
+++ Desktop/ruby_practice/submit-11.rb
@@ -80,3 +80,94 @@ for i in (1..1000)
         print number
     end
 end
+# ---------------------------------------------
+input_line = "paiza" + " "
+input_line1 = "learning"
+
+input_line << input_line1
+
+print input_line
+# ---------------------------------------------
+9.times do
+  input = gets
+  print input.chomp + " "
+end
+ input1 = gets  
+ puts input1
+# ----------------------------------------------
+
+input_line = gets.to_i
+# puts "XXXXXX"
+
+num = gets
+array = num.split #splitメソッドは文字列を分割して配列にする
+array.each do |line|   
+    puts line
+end
+# p array
+# --------------------------------------------
+input_line = gets.to_i
+input_line.times do   
+    if input_line >=2 
+        puts 1
+        puts input_line
+    
+    else
+        puts input_line
+    end
+end
+# ------------------------------------
+input_line = gets.to_i
+# input_line.times do   
+    if input_line ==2 
+        puts 1
+        puts input_line
+    
+    else
+        puts input_line
+    end
+    # ----------------------------------------
+
+input_line = gets.to_i
+# puts "XXXXXX"
+# (1..input_line).times do   
+for i in 1..input_line   
+  puts i
+end
+# ----------------------------------------------
+input_line = gets
+array = input_line.split.join(",")
+#splitメソッドは文字列を分割して配列にする
+#配列の中の用をすべて指定された引数の文字列にて結合させる
+#文字列を返す
+puts array
+
+#[1, 2, 3].join('-') #=> "1-2-3"　
+
+# --------------------------------------------------------------------------------
+input_line = gets.chomp
+# array = input_line.to_a
+array = []
+array.push(input_line)
+
+input_line2 = gets.chomp
+array.push(input_line2)
+input_line3 = gets.chomp
+array.push(input_line3)
+# p array
+
+puts array.join("|")
+#以下リファクタリング↓
+array = []
+3.times do 
+  
+    input_line = gets.chomp
+    array.push(input_line)
+end
+
+
+puts array.join("|")
+# --------------------------------------
+input_line = gets
+array = input_line.split.join(",")
+puts array + ","
