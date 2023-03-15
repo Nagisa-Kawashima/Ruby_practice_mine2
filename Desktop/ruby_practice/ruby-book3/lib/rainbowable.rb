c2f47ce9dd2b2cdce595dbf8dbf97601de8e888f
kawashima nagisa <kawashima.it333@outlook.com>
2023-03-15 16:34:03 +0900

    Edit

---
+++ Desktop/ruby_practice/ruby-book3/lib/rainbowable.rb
@@ -0,0 +1,28 @@
+module Rainbowable
+    def rainbow 
+        # str = self.to_s  
+
+        # count = 0  
+        # colored_chars = []
+
+        # str.each_char do |char|
+        #     color = 31 + count % 6
+        #     colored_chars << "\e[#{color}m#{char}"
+        #     count += 1
+        # end
+        # ret = colored_chars.join  
+        # ret + "\e[0m"
+
+        # colored_chars = []
+        # to_s.each_char.with_index do |char, count|
+        #     color = 31 + count % 6
+        #     colored_chars << "\e[#{color}m#{char}"
+        # end   
+        # colored_chars.join + "\e[0m"
+
+        to_s.each_char.map.with_index do |char, count|
+            color = 31 + count % 6
+            "\e[#{color}m#{char}"
+        end.join + "\e[0m"
+    end
+end
